11970f76d46408ecd8abab3277a67ea9bd2b82ee
Johnny Paz <johnnypaz@MacBook-Pro-de-Johnny.local>
2023-09-24 16:30:08 -0500

    Agregamos el menu de opciones de proyectos

--- Js/main.js
+++ Js/main.js
@@ -2,6 +2,9 @@ const menuCuenta = document.querySelector(`.navbar-miCuenta`);
 const fotoMenu = document.querySelector(`.fotoIconoCuenta`);
 const desktopMenu = document.querySelector(`.desktop-menu`);
 
+const OptionProyect = document.querySelector(`.icono-option`);
+const ProyectMenu = document.querySelector(`.Proyect-menu`);
+
 const IconoMenuMobil = document.querySelector(`.IconoMenuMobil`);
 const desktopMobil = document.querySelector(`.desktop-mobil`);
 
@@ -12,42 +15,55 @@ const ContentNotificaciones = document.querySelector(`.mis-notificaciones`);
 const contentNotification = document.querySelector(`.mis-notificaciones-content`);
 
 // Fecha
-const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
-const dias_semana = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+const dias_semana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 
 const fecha = new Date();
-const hoy = fecha.toDateString();
+const hoy = (dias_semana[fecha.getDay()] + ', ' + fecha.getDate() + ' de ' + meses[fecha.getMonth()]);
 
-
+const FechaHoyProyectos = document.querySelector(`.FechaHoyProyectos`);
+FechaHoyProyectos.innerText = hoy;
 
 menuCuenta.addEventListener(`click`, toggleDesktopMenu);
 fotoMenu.addEventListener(`click`, toggleDesktopMenu);
 IconoMenuMobil.addEventListener(`click`, toggleDesktopMobil);
 NotificacionesIconoMenu.addEventListener(`click`, toggleNotificacionesIconoMenu);
+OptionProyect.addEventListener(`click`, toggleProyectMenu);
+
+
+
 
 //Cerramos notificaciones  si menu de mi cuenta  esta abierto
 
 function toggleDesktopMenu() {
     const IsdesktopMobilClosed = desktopMobil.classList.contains(`inactive`);
     const IsContentNotificacionesClosed = ContentNotificaciones.classList.contains(`inactive`);
+    const IsProyectMenuClosed = ProyectMenu.classList.contains(`inactive`);
 
     if (!IsContentNotificacionesClosed){
         ContentNotificaciones.classList.add(`inactive`); 
     }
 
     if (!IsdesktopMobilClosed){
-
         desktopMobil.classList.add(`inactive`); 
     }
 
+    if (!IsProyectMenuClosed){
+        ProyectMenu.classList.add(`inactive`);
+    } 
+
+   
+
     desktopMenu.classList.toggle(`inactive`);  
 }
+
 
 //Cerramos el menu de mi cuenta si notificaciones esta abierto
 
 function toggleNotificacionesIconoMenu() {
     const IsdesktopMenuClosed = desktopMenu.classList.contains(`inactive`);
     const IsdesktopMobilClosed = desktopMobil.classList.contains(`inactive`);
+    const IsProyectMenuClosed = ProyectMenu.classList.contains(`inactive`);
 
     if (!IsdesktopMenuClosed){
 
@@ -58,7 +74,10 @@ function toggleNotificacionesIconoMenu() {
 
         desktopMobil.classList.add(`inactive`); 
     }
-    
+
+    if (!IsProyectMenuClosed){
+        ProyectMenu.classList.add(`inactive`);
+    }  
 
     ContentNotificaciones.classList.toggle(`inactive`); 
 }
@@ -68,6 +87,7 @@ function toggleNotificacionesIconoMenu() {
 function toggleDesktopMobil() {
     const IsdesktopMenuClosed = desktopMenu.classList.contains(`inactive`);
     const IsContentNotificacionesClosed = ContentNotificaciones.classList.contains(`inactive`);
+    const IsProyectMenuClosed = ProyectMenu.classList.contains(`inactive`);
 
     if (!IsdesktopMenuClosed){
 
@@ -78,8 +98,32 @@ function toggleDesktopMobil() {
         ContentNotificaciones.classList.add(`inactive`); 
     }
 
+    if (!IsProyectMenuClosed){
+        ProyectMenu.classList.add(`inactive`);
+    } 
+
     desktopMobil.classList.toggle(`inactive`);  
 }
+
+function toggleProyectMenu() {
+    const IsdesktopMenuClosed = desktopMenu.classList.contains(`inactive`);
+    const IsContentNotificacionesClosed = ContentNotificaciones.classList.contains(`inactive`);
+
+    if (!IsdesktopMenuClosed){
+
+        desktopMenu.classList.add(`inactive`); 
+    }
+
+    if (!IsContentNotificacionesClosed){
+        ContentNotificaciones.classList.add(`inactive`); 
+    }
+
+    
+    ProyectMenu.classList.toggle(`inactive`);  
+}
+
+
+
 
 
 //Notificaciones
@@ -126,6 +170,14 @@ notificationList.push({
 
 });
 
+//Capturamos la cantidad de notificaciones
+const NumeroNotification = notificationList.length;
+
+//Agregamos las cantidades a la campanita
+const CantidadNotificaciones = document.querySelector(`.CantidadNotificaciones`);
+CantidadNotificaciones.innerText = NumeroNotification;
+
+
 
 /*
 
@@ -145,61 +197,72 @@ notificationList.push({
 
 */
 
-for (notification of notificationList) {
+function MostrarNotificaciones(Not){
 
-    //Creamos el div contenedor de la notificacion
-   const notificationCard = document.createElement(`div`);
-   notificationCard.classList.add(`card-notificaciones`);
+    for (notification of Not) {
 
-   //Creamos el figure que contiene la imagen
-   const figureCard = document.createElement(`figure`);
-
-    //Creamos la imagen
-   const PhotoCard = document.createElement(`img`);
-   PhotoCard.setAttribute(`src`, notification.perfilPhoto);
-
-   //Agregamos la imagen al figure
-   figureCard.appendChild(PhotoCard);
-
-   //Creamos el parrafo de la info de la notificacion
-   const InfoCard = document.createElement(`p`);
-
-    // Creamos los span de la info
-   const NamePerfilCard = document.createElement(`span`);
-   NamePerfilCard.classList.add(`Text-Resaltado`);
-   NamePerfilCard.innerText = notification.NameUser + ` `;
-
-   const MensageNotification = document.createElement(`span`);
-   MensageNotification.innerText = notification.Mensage + ` `;
-
-   const TitleProyect = document.createElement(`span`);
-   TitleProyect.classList.add(`Text-Resaltado`);
-   TitleProyect.innerText = notification.nameProyect;
-
-    //Agreamos los span al parrafo de info
-    InfoCard.appendChild(NamePerfilCard);
-    InfoCard.appendChild(MensageNotification);
-    InfoCard.appendChild(TitleProyect);
-   
-    //Creamos el parrafo donde se puede ver el proyecto
-   const SeeMoreNotification = document.createElement(`p`);
-
-   //Creamos el enlace para ver el proyecto
-   const VerNotification = document.createElement(`a`);
-   VerNotification.innerText = `Ver`;
-   VerNotification.setAttribute(`href`, notification.UrlProyect);
-
-    //Agreamos ese enlace al parrafo
-   SeeMoreNotification.appendChild(VerNotification);
-
-    //Agregamos los elementos al div contenedor de las notificaciones
-   notificationCard.appendChild(figureCard);
-   notificationCard.appendChild(InfoCard);
-   notificationCard.appendChild(SeeMoreNotification);
-
-   //Agregamos todo al contenedor de las notificaciones
-   contentNotification.appendChild(notificationCard);
-
-
+        //Creamos el div contenedor de la notificacion
+       const notificationCard = document.createElement(`div`);
+       notificationCard.classList.add(`card-notificaciones`);
+    
+       //Creamos el figure que contiene la imagen
+       const figureCard = document.createElement(`figure`);
+    
+        //Creamos la imagen
+       const PhotoCard = document.createElement(`img`);
+       PhotoCard.setAttribute(`src`, notification.perfilPhoto);
+    
+       //Agregamos la imagen al figure
+       figureCard.appendChild(PhotoCard);
+    
+       //Creamos el parrafo de la info de la notificacion
+       const InfoCard = document.createElement(`p`);
+    
+        // Creamos los span de la info
+       const NamePerfilCard = document.createElement(`span`);
+       NamePerfilCard.classList.add(`Text-Resaltado`);
+       NamePerfilCard.innerText = notification.NameUser + ` `;
+    
+       const MensageNotification = document.createElement(`span`);
+       MensageNotification.innerText = notification.Mensage + ` `;
+    
+       const TitleProyect = document.createElement(`span`);
+       TitleProyect.classList.add(`Text-Resaltado`);
+       TitleProyect.innerText = notification.nameProyect;
+    
+        //Agreamos los span al parrafo de info
+        InfoCard.appendChild(NamePerfilCard);
+        InfoCard.appendChild(MensageNotification);
+        InfoCard.appendChild(TitleProyect);
+       
+        //Creamos el parrafo donde se puede ver el proyecto
+       const SeeMoreNotification = document.createElement(`p`);
+    
+       //Creamos el enlace para ver el proyecto
+       const VerNotification = document.createElement(`a`);
+       VerNotification.innerText = `Ver`;
+       VerNotification.setAttribute(`href`, notification.UrlProyect);
+    
+        //Agreamos ese enlace al parrafo
+       SeeMoreNotification.appendChild(VerNotification);
+    
+        //Agregamos los elementos al div contenedor de las notificaciones
+       notificationCard.appendChild(figureCard);
+       notificationCard.appendChild(InfoCard);
+       notificationCard.appendChild(SeeMoreNotification);
+    
+       //Agregamos todo al contenedor de las notificaciones
+       contentNotification.appendChild(notificationCard);
+    }
 }
+
+MostrarNotificaciones(notificationList);
+
+//Feche Agregada a las natificaciones
+
+const FechaHoy = document.querySelector(`.FechaHoy`);
+FechaHoy.innerText = hoy;
+
+
+
 
